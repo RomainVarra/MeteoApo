89d753c4484d451ccb65913fd7c64cb11d28feb6
Romain Varra <romain.varra@hotmail.com>
2024-12-03 12:21:31 +0100

    feat: router creation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import Homepage from './pages/Homepage.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage/>,
+      }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
